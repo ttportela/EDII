660102f7a51b3b643ecc033efe7d6cdeeb89d2f8
Tarlis Portela <tarlis@tarlis.com.br>
2023-10-21 01:22:39 +0000

    Aula 20-10

--- ArvoreIdade.py
+++ ArvoreIdade.py
@@ -14,7 +14,7 @@ class Node:
     
     def add(self, value): # BFS - Busca em Largura
         new_node = Node(value)
-        print(self.value, ".add:", value)
+        #print(self.value, ".add:", value)
         
         if value < self.value:
             if self.left == None:
@@ -30,14 +30,30 @@ class Node:
     def addPessoa(self, P):
         if self.value == P.idade:
             self.pessoas.append(P)
-        elif self.value < P.idade:
+        elif self.value > P.idade:
             if self.left == None:
                 self.left = Node(P.idade)
             self.left.addPessoa(P)
-        else: #elif self.value > P.idade:
+        else: #elif self.value < P.idade:
             if self.right == None:
                 self.right = Node(P.idade)
             self.right.addPessoa(P)
+    
+    def imprimir(self):
+        #Na ORDEM:
+        # 1° Esquerda
+        if self.left:
+            self.left.imprimir()
+
+        #2° O NÓ:
+        print("Idade ", self.value)
+        for p in self.pessoas:
+            print(" - ", p.nome, "|", p.idade)
+
+        #3° Direito:
+        if self.right:
+            self.right.imprimir()
+
 # ------
 root = Node(50)
 print("ADD 35:")
@@ -55,3 +71,12 @@ print("-------------------")
 print("ADD 60:")
 root.add(60)
 print("-------------------")
+
+root.addPessoa( Pessoa("João", 37) )
+root.addPessoa( Pessoa("José", 37) )
+root.addPessoa( Pessoa("Maria", 20) )
+root.addPessoa( Pessoa("Steven", 60) )
+root.addPessoa( Pessoa("Enzo", 10) )
+
+print("------------------- -------------------")
+root.imprimir()
